75a3b29f2b03b5a219323e3d13c82b44577c754c
Mirko Cukich <mirkocukich@gmail.com>
2018-03-24 13:14:57 -0500

    Program Complete

--- app/src/main/java/com/digitalmirko/autoimageflipperlibrarydemo/MainActivity.java
+++ app/src/main/java/com/digitalmirko/autoimageflipperlibrarydemo/MainActivity.java
@@ -16,40 +16,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final FlipperLayout flipperLayout = (FlipperLayout) findViewById(R.id.flipper_layout);
+        // using 10 images on the app
         int num_of_pages = 10;
         for (int i = 0; i < num_of_pages; i++) {
             FlipperView view = new FlipperView(getBaseContext());
 //            view.setImageDrawable(R.drawable.image1) // Use one of setImageUrl() or setImageDrawable() functions, otherwise IllegalStateException will be thrown
 
+            // 10 images available in the demo
+            // images found on unsplash.com, author details included below about images used
             switch(i){
                 case 0:
+                    // Mosque down view - Photo by Afifi Zulkifle on Unsplash
                     view.setImageDrawable(R.drawable.image1);
                     break;
                 case 1:
+                    // New York Skyscrapers - Photo by Ryan Loughlin on Unsplash
                     view.setImageDrawable(R.drawable.image2);
                     break;
                 case 2:
+                    // Chicago Skyscrapers looking up at a plane - Photo by emily reider on Unsplash
                     view.setImageDrawable(R.drawable.image3);
                     break;
                 case 3:
+                    // Escalator - Photo by Jorik Kleen Unsplash
                     view.setImageDrawable(R.drawable.image4);
                     break;
                 case 4:
+                    // Singapore City Skyline - Photo by Xavier Lee on Unsplash
                     view.setImageDrawable(R.drawable.image5);
                     break;
                 case 5:
+                    // Asign Market - Photo by Alec Webskowski on Unsplash
                     view.setImageDrawable(R.drawable.image6);
                     break;
                 case 6:
+                    // Woman laying back on PacMan Screen - Photo by Erik Lucatero on Unsplash
                     view.setImageDrawable(R.drawable.image7);
                     break;
                 case 7:
+                    // Tennis Court down view - Photo by Izuddin Helmi Adnan on Unsplash
                     view.setImageDrawable(R.drawable.image8);
                     break;
                 case 8:
+                    // Cave opening at night Jonatan Pie on Unsplash
                     view.setImageDrawable(R.drawable.image9);
                     break;
                 case 9:
+                    // Brooklyn Winter, United States - Photo by Andre Benz on Unsplash
                     view.setImageDrawable(R.drawable.image10);
                     break;
             }
@@ -61,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
                         public void onFlipperClick(FlipperView flipperView) {
                             //Handle View Click here
                             int position = flipperLayout.getCurrentPagePosition() + 1;
+                            // Toast to show Image Position when user clicks the photo
                             Toast.makeText(MainActivity.this, "Position " + position,
                                     Toast.LENGTH_SHORT).show();
                         }
